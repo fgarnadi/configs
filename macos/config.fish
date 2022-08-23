7dc063f90b26537a2fc9803ef69b7fb61e3bb189
fgarnadi <fajar.garnadi@gmail.com>
2022-08-23 09:28:57 +0700

    update macos 23/08/2022

--- macos/config.fish
+++ macos/config.fish
@@ -1,7 +1,13 @@
+# remove fish greeting
+set -g fish_greeting
+
+# homebrew setup
+eval (/opt/homebrew/bin/brew shellenv)
+
 starship init fish | source
 
 # sbin path
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+fish_add_path -g /usr/local/sbin
 
 # Locale
 set -x LC_ALL en_US.UTF-8
@@ -11,12 +17,29 @@ set -x LANG en_US.UTF-8
 # alias activate='source venv/bin/activate'
 
 # Python pyenv
-pyenv init - | source
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PYTHON_CONFIGURE_OPTS --enable-framework
+fish_add_path -g $PYENV_ROOT/bin
+
+
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
 
 # Node nodenv
-eval (nodenv init - | source)
+status --is-interactive; and source (nodenv init -|psub)
+
+# Java jenv
+# set -x JENV_ROOT $HOME/.jenv
+# fish_add_path -g $JENV_ROOT/bin
+
+# status --is-interactive; and source (jenv init -|psub)
+
+# Rust
+set -x CARGO_HOME $HOME/.cargo
+fish_add_path -g $CARGO_HOME/bin
 
 # Alias
+alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
 alias brewit='update; upgrade; brew cleanup; brew doctor'
 alias update="brew update"
 alias upgrade="brew upgrade"
@@ -36,23 +59,25 @@ alias infoc="brew info --cask"
 alias zapc="brew remove --zap --cask"
 
 alias ls="exa"
+alias vim="nvim"
 
-# Home variable
-set -x GOPATH $HOME/Code/go
+# Golang
+# set -x GOPATH $HOME/Code/go
 # set -x GOBIN $GOPATH/bin 
-set -x GOVERSION (go version | grep -o '\([0-9]\.\?\)\+' | head -1)
-set -x GOROOT /usr/local/Cellar/go/$GOVERSION/libexec 
+# set -x GOVERSION (go version | grep -o '\([0-9]\.\?\)\+' | head -1)
+# set -x GOROOT /usr/local/Cellar/go/$GOVERSION/libexec 
+# set -x PATH $GOBIN $PATH
 
 # PATH variable
-# set -x PATH $GOBIN $PATH
-set -x PATH $HOME/.cargo/bin $PATH
-set -x PATH $HOME/.flutter/bin $PATH
-set -x PATH $HOME/Library/Android/sdk/tools $PATH
-set -x PATH $HOME/Library/Android/sdk/tools/bin $PATH
-set -x PATH $HOME/Library/Android/sdk/platform-tools $PATH
+set -x PATH $HOME/.local/bin $PATH
+
+# Android sdk
+set -x PATH $HOME/.android/sdk/tools $PATH
+set -x PATH $HOME/.android/sdk/tools/bin $PATH
+set -x PATH $HOME/.android/sdk/platform-tools $PATH
 
 # GCP
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
+# source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
 
 # Misc
-set -x EDITOR /usr/local/bin/vim
+set -x EDITOR (which vim)
